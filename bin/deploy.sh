33365eeb3fbd96bac39d9eb7ad343d676276343e
Jonathan Morris <jonathan@revcaster.com>
2017-06-15 12:46:46 -0700

    tweak

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,12 +3,15 @@
 set -e
 
 
+deploy_folder='deploy/'
+
+
 check_deploy_changes() {
     # Does diff of current commit with last merge commit for the deploy/ directory.
     # This makes sure that a new image is deployed to ECR only when there has been
     # a change to the Dockerfile, the docker-compose.yml, or something that goes into it.
     last_merge_commit=$(git --no-pager log --pretty=oneline --format=format:%H --merges -n 1)
-    DIFF=$(git diff $last_merge_commit -- deploy/)
+    DIFF=$(git diff $last_merge_commit -- $deploy_folder/)
 
     echo $DIFF
 
